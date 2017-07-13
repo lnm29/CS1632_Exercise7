be8028ab58663fda345be70395af6ae81e85c0c5
lnm29 <lnm29@pitt.edu>
2017-07-13 08:07:28 -0400

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -11,7 +11,7 @@ public class TestRunner {
 
  // ADD ANY MORE CLASSES YOU WISH TO TEST HERE
  
- classesToTest.add(Deliverable3Test.class);
+ classesToTest.add(MonkeySimTest.class);
  
  // For all test classes added, loop through and use JUnit
  // to run them.
